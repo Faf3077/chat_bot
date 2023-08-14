ed5051edd235468f904bae39df38feb90a2d3030
Андрей <see320m@gmail.com>
2023-08-14 15:52:23 +0300

    мелкие доработки

--- src/components/windowChat.vue
+++ src/components/windowChat.vue
@@ -1,31 +1,65 @@
 <template>
   <div class="chat-widget">
-    <div class="chat-messages">
-      <div v-for="message in messages" :key="message.id" class="message" :class="{ 'user-message': message.user === 'user', 'bot-message': message.user === 'bot' }">{{ message.text }}</div>
+    <div class="chat-messages" ref="chatMessages">
+      <div v-for="message in messages" :key="message.id" class="message-container">
+        <div v-if="message.user === 'user'" class="user-message">{{ message.text }}</div>
+        <div class="bot" v-else><img src="../assets/robot.png" alt="">
+          <div class="bot-message">{{ message.text }}</div>
+        </div>
+      </div>
     </div>
     <div class="options">
-      <button v-for="option in options" :key="option.id" @click="selectOption(option)" class="option">{{ option.text }}</button>
+      <button v-for="option in options" :key="option.id" @click="selectOption(option)" class="option">{{ option.text
+      }}</button>
     </div>
   </div>
 </template>
 
 <script>
+import { ref, onMounted } from 'vue';
+
 export default {
-  data() {
-    return {
-      messages: [],
-      options: [
-        { id: 1, text: 'Привет', response: 'Здравствуйте! Как я могу помочь вам?' },
-        { id: 2, text: 'Как дела?', response: 'У меня всё хорошо, спасибо за вопрос!' },
-        { id: 3, text: 'Пока', response: 'До свидания! Приходите ещё!' }
-      ]
+  setup() {
+    const messages = ref([]);
+    const options = [
+      { id: 1, text: 'Как дела?', response: 'У меня всё хорошо, спасибо за вопрос!' },
+      { id: 2, text: 'Пока', response: 'До свидания! Приходите ещё!' }
+    ];
+
+    const chatMessages = ref(null); // Референс на элемент с сообщениями
+
+    const selectOption = async (option) => {
+      messages.value.push({ text: option.text, user: 'user' });
+
+      // Анимация печатания для бота
+      messages.value.push({ text: '...', user: 'bot', isTyping: true });
+      await new Promise(resolve => setTimeout(resolve, 500));
+
+      messages.value.pop(); // Удаляем многоточие
+
+      messages.value.push({ text: option.response, user: 'bot' });
+
+      scrollToBottom(); // Прокрутка чата вниз
     };
-  },
-  methods: {
-    selectOption(option) {
-      this.messages.push({ text: option.text, user: 'user' });
-      this.messages.push({ text: option.response, user: 'bot' });
-    }
+
+    const scrollToBottom = () => {
+      if (chatMessages.value) {
+        chatMessages.value.scrollTop = chatMessages.value.scrollHeight;
+      }
+    };
+
+    messages.value.push({ text: 'Здравствуйте! Чем я могу вам помочь?', user: 'bot' });
+
+    onMounted(() => {
+      scrollToBottom(); // Прокрутка вниз при загрузке компонента
+    });
+
+    return {
+      messages,
+      options,
+      selectOption,
+      chatMessages
+    };
   }
 };
 </script>
@@ -40,6 +74,7 @@ $primary-hover-color: #45a049;
 .chat-widget {
   width: 100%;
   max-width: 400px;
+  max-height: 500px;
   border: 1px solid #ccc;
   border-radius: 5px;
   overflow: hidden;
@@ -47,51 +82,69 @@ $primary-hover-color: #45a049;
   flex-direction: column;
   margin: 0 auto;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-}
 
-.chat-messages {
-  flex: 1;
-  overflow-y: scroll;
-  padding: 10px;
-}
+  .chat-messages {
+    flex: 1;
+    overflow-y: scroll;
+    padding: 10px;
 
-.message {
-  padding: 10px;
-  border-radius: 5px;
-  margin: 5px;
-  word-wrap: break-word;
+    .message-container {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      margin-bottom: 10px;
 
-  &.user-message {
-    align-self: flex-end;
-    background-color: $user-message-bg;
+      .user-message {
+        align-self: flex-end;
+        background-color: $user-message-bg;
+        padding: 10px;
+        border-radius: 5px;
+        word-wrap: break-word;
+      }
+
+      .bot {
+        display: flex;
+        gap: 10px;
+        align-items: center;
+
+        .bot-message {
+          background-color: $bot-message-bg;
+          color: $bot-message-text-color;
+          padding: 10px;
+          border-radius: 5px;
+          word-wrap: break-word;
+        }
+      }
+    }
   }
 
-  &.bot-message {
-    align-self: flex-start;
-    background-color: $bot-message-bg;
-    color: $bot-message-text-color;
-  }
-}
+  .options {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 10px;
+    background: #dddcdc;
 
-.options {
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  padding: 10px;
-}
+    .option {
+      margin: 5px;
+      padding: 8px 15px;
+      background-color: $primary-color;
+      color: white;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+      transition: background-color 0.2s;
 
-.option {
-  margin: 5px;
-  padding: 8px 15px;
-  background-color: $primary-color;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  transition: background-color 0.2s;
-
-  &:hover {
-    background-color: $primary-hover-color;
+      &:hover {
+        background-color: $primary-hover-color;
+      }
+    }
   }
 }
 </style>
+
+
+
+
+
+
